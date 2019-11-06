8aea3bd9568fe940245da95d6e36b6b32ed93f45
sabljica <tarik.sabljica7@gmail.com>
2019-11-07 00:59:14 +0100

    commit 1

---
+++ Controllers/AccController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Practice.Models;
+using System.Data.SqlClient;
+namespace Practice.Controllers
+{
+    public class AccController : Controller
+    {
+        SqlConnection con = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+        // GET: Acc
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+        void connectionString()
+        {
+            con.ConnectionString = "data source=DESKTOP-EKLDOLV; database=WPF; integrated security= SSPI;";
+        }
+        [HttpPost]
+        public ActionResult Verify(Acc akaunt)
+        {
+            connectionString();
+            con.Open();
+            com.Connection = con;
+            com.CommandText = "select * from tbl_login where username='"+akaunt.User+"' and password='"+akaunt.Password+"'";
+            dr = com.ExecuteReader();
+            if(dr.Read())
+            {
+                con.Close();
+                return View("Create");
+            }
+            else
+            {
+                con.Close();
+                return View("Fail");
+            }
+
+            
+           
+        }
+    }
+}
---
+++ Models/Acc.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Practice.Models
+{
+    public class Acc
+    {
+        public string User { get; set; }
+        public string Password { get; set; }
+
+    }
+}
